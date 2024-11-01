c6b7af575481db4551cba9aee66188c5b370f3a1
Rastin Badpeyma <rastinbadpeyma@gmail.com>
2024-11-01 17:46:53 +0330

    Custom CurrentUser

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthGuard } from "@nestjs/passport";
+import { CurrentUser } from "./current-user.decorator";
+import { User } from "./entities/user.entity";
 
 
 @Controller('auth')
@@ -11,19 +13,19 @@ export class AuthController {
 
    @Post('login')
    @UseGuards(AuthGuard('local'))
-   async login(@Request() request){
+   async login(@CurrentUser() user:User){
       console.log(process.env.AUTH_SECRET);
 
       return {
-         userId: request.user.id,
-         token: this.authService.getTokenForUser(request.user)
+         userId: user.id,
+         token: this.authService.getTokenForUser(user)
       }
 
    }
 
    @Get('profile')
    @UseGuards(AuthGuard('jwt'))
-   async getProfile(@Request() request){
-    return request.user
+   async getProfile(@CurrentUser() user:User){
+    return user;
    }
 }
