20ffa1734194810d99fa4aec917efe2eea456c3e
Rastin Badpeyma <rastinbadpeyma@gmail.com>
2024-11-01 19:22:41 +0330

    User Registration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,24 @@
-import { Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { AuthGuard } from "@nestjs/passport";
 import { CurrentUser } from "./current-user.decorator";
 import { User } from "./entities/user.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { CreateUserDto } from "./input/create.user.dto";
+import { AuthGuardLocal } from "./guards/auth-guard.local";
+import { AuthGuardJwt } from "./guards/auth-guard.jwt";
 
 
 @Controller('auth')
 export class AuthController {
    constructor(
-    private readonly authService: AuthService
+    private readonly authService: AuthService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
    ){}
 
    @Post('login')
-   @UseGuards(AuthGuard('local'))
+   @UseGuards(AuthGuardLocal)
    async login(@CurrentUser() user:User){
       console.log(process.env.AUTH_SECRET);
 
@@ -23,8 +29,39 @@ export class AuthController {
 
    }
 
+     @Post('register')
+     async register(@Body() createUserDto: CreateUserDto){
+      const user = new User();
+
+      if (createUserDto.password !== createUserDto.retypedPassword) {
+         throw new BadRequestException(['Passwords are not identical']);
+      }
+      
+      const existingUser = await this.userRepository.findOne({
+         where: [
+            {username: createUserDto.username},
+            {email: createUserDto.email}
+         ]
+      });
+
+      if (existingUser) {
+         throw new BadRequestException(['username or email is already taken'])
+      }
+
+      user.username = createUserDto.username;
+      user.password = await this.authService.hashPassword(createUserDto.password);
+      user.email = createUserDto.email;
+      user.firstName = createUserDto.firstName;
+      user.lastName = createUserDto.lastName;
+
+      return {
+         ...(await this.userRepository.save(user)),
+         token: this.authService.getTokenForUser(user)
+      }
+     }
+   
    @Get('profile')
-   @UseGuards(AuthGuard('jwt'))
+   @UseGuards(AuthGuardJwt)
    async getProfile(@CurrentUser() user:User){
     return user;
    }
